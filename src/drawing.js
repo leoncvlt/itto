8f2998afda14471fabd4d180ea33072dad6ebed1
Leonardo Cavaletti <impeto.blu@gmail.com>
2022-03-27 14:58:15 +0100

    Added rounding to drawing coordinates

--- src/drawing.js
+++ src/drawing.js
@@ -14,8 +14,8 @@ const line = (x0, y0, x1, y1, color) => {
   const { context } = itto;
   context.strokeStyle = color;
   context.beginPath();
-  context.moveTo(x0, y0);
-  context.lineTo(x1, y1);
+  context.moveTo(Math.round(x0), Math.round(y0));
+  context.lineTo(Math.round(x1), Math.round(y1));
   context.stroke();
 };
 
@@ -23,17 +23,17 @@ const rect = (x, y, w, h, color, border) => {
   const { context } = itto;
   if (border) {
     context.strokeStyle = color;
-    context.strokeRect(x + 0.5, y + 0.5, w, h);
+    context.strokeRect(Math.round(x) + 0.5, Math.round(y) + 0.5, w, h);
   } else {
     context.fillStyle = color;
-    context.fillRect(x, y, w, h);
+    context.fillRect(Math.round(x), Math.round(y), w, h);
   }
 };
 
 const circ = (x, y, r, color, border) => {
   const { context } = itto;
   context.beginPath();
-  context.arc(x, y, r, 0, Math.PI * 2);
+  context.arc(Math.round(x), Math.round(y), r, 0, Math.PI * 2);
   if (border) {
     context.strokeStyle = color;
     context.stroke();
@@ -45,14 +45,14 @@ const circ = (x, y, r, color, border) => {
 
 const spr = (spritesheet, sx = 0, sy = 0, w = 0, h = 0, x = 0, y = 0) => {
   const { context, assets } = itto;
-  context.drawImage(assets[spritesheet], sx, sy, w, h, x, y, w, h);
+  context.drawImage(assets[spritesheet], sx, sy, w, h, Math.round(x), Math.round(y), w, h);
 };
 
 const print = (text, x = 0, y = 0, color = "black", size = 8, font = "itto") => {
   const { context } = itto;
   context.font = `${size}px ${font}`;
   context.fillStyle = color;
-  context.fillText(text, x, y);
+  context.fillText(text, Math.round(x), Math.round(y));
 };
 
 export { cls, spr, line, rect, circ, print };
