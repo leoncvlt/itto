ca3ec945eda110f277b56adc8380824e0836eabc
Leonardo Cavaletti <impeto.blu@gmail.com>
2022-03-27 14:59:04 +0100

    Added extensions and audio handling in assets

--- src/assets.js
+++ src/assets.js
@@ -1,11 +1,24 @@
-const loadAsset = (path) =>
-  new Promise((resolve) => {
-    if (path.endsWith("png")) {
+import { audioContext } from "./audio";
+
+const testExtension = (path, extensions = []) => {
+  const extension = path.substring(path.lastIndexOf(".") + 1, path.length) || path;
+  return extensions.includes(extension.toLowerCase());
+};
+
+const loadAsset = async (path) => {
+  if (testExtension(path, ["jpg", "jpeg", "png"])) {
+    return new Promise((resolve) => {
       const image = new Image();
       image.onload = resolve(image);
       image.src = path;
-    }
-  });
+    });
+  } else if (testExtension(path, ["wav", "mp3", "ogg"])) {
+    const response = await fetch(path);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+  }
+};
 
 export const loadAssets = async (assets, store) => {
   const promises = Object.entries(assets).map(
